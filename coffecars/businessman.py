976e697fb1f6e26ae4732f37f752a83abf3f736c
RomCodeman <codemanners@gmail.com>
2020-08-29 13:49:20 +0300

    cars | get_Filter businessman | something

--- coffecars/businessman.py
+++ coffecars/businessman.py
@@ -1,6 +1,8 @@
 from coffecars import CofeCar, Drink
 from typing import List
 
+from coffecars.coord import Coord
+
 
 class BusinessOwner:
     def __init__(self, name=""):
@@ -20,12 +22,33 @@ class BusinessOwner:
     def remDrinks(self):
         pass
 
-    def filterCofeCars(self, adress) -> List[CofeCar]:
-        return []
+    def filterCofeCars(self, values: dict) -> List[CofeCar]:
+
+        def getFilter():
+            if values.get("address"):
+                def get_address(car):
+                    if values.get("address").lower() in car.address.lower():
+                        return True
+                    else:
+                        return False
+                return get_address
+            elif values.get("coord"):  # {"x": 1, "y": 2, "radius": 100}
+                def get_coord(car):
+                    coord = Coord(values.get("coord").get("x"), values.get("coord").get("y"))
+                    distance = coord.distance_to(car.coord)
+                    radius = values.get("coord").get("radius")
+                    if distance <= radius:
+                        return True
+                    else:
+                        return False
+                return get_coord
+        return [car for car in self.cofe_cars if getFilter()(car)]
+
+
 
     def filterDrinks(self, name) -> List[Drink]:
         return []
 
-    def applyDrinkstoCar(self, drinks: Drink, cars: CofeCar, flag):
+    def applyDrinkstoCar(self, drinks: Drink, cars: List[CofeCar], flag):
         for car in cars:
-            pass
+            car.applyDrinks(drinks, flag)
